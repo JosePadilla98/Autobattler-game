308c74ae690634b35d6a34f4f658ad284e548053
José Padilla <40579916+JosePadilla98@users.noreply.github.com>
2022-05-31 14:42:42 +0200

    Progress

---
+++ Autobattler/Assets/Code/MutationsSystem/Mutations/Others/StandardMutation.cs
@@ -0,0 +1,36 @@
+using System;
+using Autobattler.Units;
+using Autobattler.Units.Combat;
+using Autobattler.Units.Combat.CombatSystems;
+using Autobattler.Units.Management;
+using UnityEngine;
+
+namespace Autobattler.MutationsSystem.Mutations.Attacks
+{
+    [CreateAssetMenu(fileName = "StandardMutation", menuName = "ScriptableObjects/MutationsSystem/Mutations/Standard")]
+    public class StandardMutation : MutationModel
+    {
+        [SerializeField] 
+        protected StatModifier[] statModifiers;
+
+        public override void ModifyStats(Stats stats)
+        {
+            Stats.ApplyStatsModifiers(statModifiers, stats);
+        }
+
+        public override void UnmodifyStats(Stats stats)
+        {
+            Stats.UnapplyStatsModifiers(statModifiers, stats);
+        }
+
+        public override void AttachToCombatModules(int order, int key, Fighter unit)
+        {
+
+        }
+        
+        public override void UnattachToCombatModules(int key, Fighter unit)
+        {
+
+        }
+    }
+}
